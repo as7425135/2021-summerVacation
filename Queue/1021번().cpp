a143e18d88c2be2127e73a30db07ba787a254dd2
as7425135 <62742722+as7425135@users.noreply.github.com>
2021-07-12 01:29:17 +0900

    Add files via upload

---
+++ Queue/1021번().cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+ 
+int N, M;
+deque<int> dq;
+ 
+int main(void)
+{
+        cin >> N >> M;
+ 
+        for (int i = 1; i <= N; i++)
+                 dq.push_back(i);
+ 
+        int result = 0;
+        for (int i = 0; i < M; i++)
+        {
+                 int idx;
+                 cin >> idx;
+ 
+                 int cur = 1;
+                 //뽑아내고자 하는 수의 위치
+                 for (deque<int>::iterator iter = dq.begin(); iter < dq.end(); iter++)
+                 {
+                         if (*iter == idx)
+                                 break;
+                         cur++;
+                 }
+ 
+                 //왼쪽으로 몇 번 쉬프트?
+                 int left = cur - 1;
+                 //오른쪽으로 몇 번 쉬프트?
+                 int right = dq.size() - cur + 1;
+ 
+                 if (left < right)
+                 {
+                         for (int j = 1; j <= left; j++)
+                         {
+                                 int num = dq.front();
+                                 dq.pop_front();
+                                 dq.push_back(num);
+                                 result++;
+                         }
+                         dq.pop_front();
+                 }
+                 else
+                 {
+                         for (int j = 1; j <= right; j++)
+                         {
+                                 int num = dq.back();
+                                 dq.pop_back();
+                                 dq.push_front(num);
+                                 result++;
+                         }
+                         dq.pop_front();
+                 }
+        }
+ 
+        cout << result << endl;
+        return 0;
+}
+
+
+
+출처: https://jaimemin.tistory.com/688 [꾸준함]
